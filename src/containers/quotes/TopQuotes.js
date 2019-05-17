49d3b59459eebf89e7ddc5ac262404d658f4d81d
Cheri Portman <cheriportman@hotmail.com>
2019-05-17 09:39:19 -0700

    wrote Loading.js

--- src/containers/quotes/TopQuotes.js
+++ src/containers/quotes/TopQuotes.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import Quotes from '../../components/Quotes';
+import Quotes from '../../components/quotes/Quotes';
 
 
 export default class TopQuotes extends PureComponenet {
