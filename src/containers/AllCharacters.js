9fff06fc6f10a706ee39960bb28ae17e6a4ade6c
Cheri Portman <cheriportman@hotmail.com>
2019-05-18 10:11:56 -0700

    AllCharacters written

--- src/containers/AllCharacters.js
+++ src/containers/AllCharacters.js
@@ -1,4 +1,28 @@
 import React from 'react';
 import Characters from '../components/characters/Characters';
 import Loading from '../components/Loading';
+import { getCharacters } from '../services/rickAndMortyApi';
 
+export default class AllCharacters extends PureComponenet {
+  state = {
+    characters: [],
+    loading: true
+  }
+
+  fetchCharacters = () => {
+    this.setState({ loading: true });
+    getCharacters()
+      .then(({ characters }) => this.setState({ characters, loading: false }));
+  }
+
+  componenetDidMount() {
+    this.fetchCharacters();
+  }
+
+  render() {
+    const { characters, loading } = this.state;
+    if(loading) return <Loading />;
+
+    return <Characters characters={characters} />;
+  }
+}
