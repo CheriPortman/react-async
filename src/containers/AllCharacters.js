964bcd6f44be77732803f4eb985b4d4c27b57cc4
Cheri Portman <cheriportman@hotmail.com>
2019-05-18 15:14:41 -0700

    paging buttons in AllCharacters.js increment and decrement

--- src/containers/AllCharacters.js
+++ src/containers/AllCharacters.js
@@ -3,26 +3,57 @@ import Characters from '../components/characters/Characters';
 import Loading from '../components/Loading';
 import { getCharacters } from '../services/rickAndMortyApi';
 
+//add page to state and create buttons ti increment and decrement page
+//use page to page through api
+//can't to past last page
+//can't go before first page
+//make a paging componenet
+
 export default class AllCharacters extends PureComponent {
   state = {
     characters: [],
-    loading: true
+    loading: true,
+    currentPage: 1,
+    totalPages: 1
   }
   
   fetchCharacters = () => {
     this.setState({ loading: true });
-    getCharacters()
-      .then(({ characters }) => this.setState({ characters, loading: false }));
+    getCharacters(this.state.currentPage)
+      .then(({ totalPages, characters }) => this.setState({ 
+        totalPages,
+        characters, 
+        loading: false 
+      }));
   }
   
+  nextPage = page => {
+    this.setState({ currentPage: page });
+  }
+
   componentDidMount() {
     this.fetchCharacters();
   }
 
   render() {
-    const { characters, loading } = this.state;
+    const { 
+      characters, 
+      loading,
+      currentPage,
+      totalPages
+    } = this.state;
     if(loading) return <Loading />;
 
-    return <Characters characters={characters} />;
+    return (
+      <>
+        {currentPage > 1 && 
+          <button onClick={() => this.nextPage(currentPage - 1)}>Previous Page</button>}
+        {`${currentPage} / ${totalPages}`}
+        {currentPage < totalPages &&
+          <button onClick={() => this.nextPage(currentPage + 1)}>Next Page</button>}
+          {/* {children} */}
+        <Characters characters={characters} />
+      </>
+    );
   }
 }
